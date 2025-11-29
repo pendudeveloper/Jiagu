dae5b3ed0cfc8455d04f4f72556a181368cba8a9
pendudeveloper <145433947+pendudeveloper@users.noreply.github.com>
2025-11-29 14:24:24 +0500

    Update SignUtils.java

--- pack/src/main/java/com/frezrik/jiagu/pack/util/SignUtils.java
+++ pack/src/main/java/com/frezrik/jiagu/pack/util/SignUtils.java
@@ -1,16 +1,10 @@
 package com.frezrik.jiagu.pack.util;
 
-import com.android.apksig.ApkSigner;
-import com.android.apksig.apk.ApkCreatorFactory;
-import com.android.apksig.apk.ApkZFileCreator;
-import com.android.apksig.apk.ApkZFileCreatorFactory;
-import com.android.apksig.zip.ZipFormatException;
+import com.frezrik.jiagu.pack.core.AppManager;
 
-import java.io.*;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.cert.Certificate;
-import java.util.Collections;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class SignUtils {
 
@@ -19,133 +13,47 @@ public class SignUtils {
     }
 
     /**
-     * Keep SAME API
-     * V1 signing (using ApkSigner V1 mode internally)
+     * V1签名
      */
-    private static String signature(
-            File unsignedApk,
-            String keyStore,
-            String keyPwd,
-            String alias,
-            String alisaPwd
-    ) throws IOException {
+    private static String signature(File unsignedApk, String keyStore, String keyPwd,
+                                    String alias, String alisaPwd)
+            throws InterruptedException, IOException {
+        String path = unsignedApk.getAbsolutePath();
+        String v1Name = path.substring(0, path.indexOf(".apk")) + "_v1.apk";
+        String cmd = AppManager.BIN_RUNNER + "jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore " + keyStore + " -storepass " + keyPwd +
+                " -keypass " + alisaPwd + " -signedjar " + v1Name + " " + unsignedApk.getAbsolutePath() + " " + alias;
 
-        String v1Name = unsignedApk.getAbsolutePath().replace(".apk", "_v1.apk");
+        CmdUtils.exec("v1 sign", cmd);
 
-        try {
-            // Load keystore
-            KeyStore ks = KeyStore.getInstance("JKS");
-            try (FileInputStream fis = new FileInputStream(keyStore)) {
-                ks.load(fis, keyPwd.toCharArray());
-            }
-
-            PrivateKey key = (PrivateKey) ks.getKey(alias, alisaPwd.toCharArray());
-            Certificate[] certs = ks.getCertificateChain(alias);
-
-            ApkSigner.SignerConfig signerConfig =
-                    new ApkSigner.SignerConfig.Builder(alias, key, Collections.singletonList(certs[0]))
-                            .build();
-
-            // Use ApkSigner to perform V1 signing
-            ApkSigner signer = new ApkSigner.Builder(Collections.singletonList(signerConfig))
-                    .setInputApk(unsignedApk)
-                    .setOutputApk(new File(v1Name))
-                    .setV1SigningEnabled(true)
-                    .setV2SigningEnabled(false)
-                    .setV3SigningEnabled(false)
-                    .build();
-
-            signer.sign();
-
-        } catch (Exception e) {
-            throw new IOException("V1 signing failed", e);
-        }
+        FileUtils.delete(path);
 
         return v1Name;
     }
 
-    /**
-     * SAME API
-     * zipalign 4 bytes using ApkZlib (no external command)
-     */
-    private static String apkZipalign(String v1Apk) throws IOException {
-        String aligned = v1Apk.replace(".apk", "_align.apk");
+    // zipalign -p 4 input\app-release-unsigned.apk input\app-release-unsigned.apk
+    private static String apkZipalign(String v1Apk) throws IOException, InterruptedException {
+        String zipalignName = v1Apk.substring(0, v1Apk.indexOf(".apk")) + "_align.apk";
+        String cmd = AppManager.BIN_RUNNER  + AppManager.BIN_PATH + "zipalign -p 4 " + v1Apk + " " + zipalignName;
 
-        try {
-            ApkZFileCreatorFactory.ZipFileAlignment alignment =
-                    new ApkZFileCreatorFactory.ZipFileAlignment(4);
+        CmdUtils.exec("zipalign", cmd);
 
-            ApkZFileCreatorFactory factory = new ApkZFileCreatorFactory();
-            ApkCreatorFactory.ApkCreator creator =
-                    factory.make(new File(aligned), alignment);
+        FileUtils.delete(v1Apk);
 
-            try (ApkZFileCreatorFactory.ApkZFileCreator z =
-                         (ApkZFileCreatorFactory.ApkZFileCreator) creator) {
-
-                z.writeZip(new File(v1Apk));
-            }
-
-        } catch (ZipFormatException e) {
-            throw new IOException("Zipalign failed", e);
-        }
-
-        return aligned;
+        return zipalignName;
     }
 
-    /**
-     * SAME API
-     * Full signing: V1 + V2 + V3
-     */
-    public static void apkSignature(
-            File unsignedApk,
-            File signedApk,
-            String keyStore,
-            String keyPwd,
-            String alias,
-            String alisaPwd
-    ) throws IOException {
+    //apksigner sign  --ks key.jks --ks-key-alias releasekey  --ks-pass pass:pp123456
+    // --key-pass pass:pp123456  --out output.apk  input.apk
+    public static void apkSignature(File unsignedApk, File signedApk, String keyStore,
+                                    String keyPwd, String alias, String alisaPwd) throws IOException, InterruptedException {
+        String v1Name = signature(unsignedApk, keyStore, keyPwd, alias, alisaPwd);
+        String zipalignName = apkZipalign(v1Name);
+        String cmd = AppManager.CMD_RUNNER +  AppManager.BIN_PATH + "apksigner sign --ks " + keyStore + " --ks-pass pass:" + keyPwd
+                + " --ks-key-alias " + alias + " --key-pass pass:" + alisaPwd + " --out " + signedApk.getAbsolutePath() + " " + zipalignName;
 
-        // Step 1: V1 signing
-        String v1Apk = signature(unsignedApk, keyStore, keyPwd, alias, alisaPwd);
+        CmdUtils.exec("v2 sign", cmd);
 
-        // Step 2: zipalign
-        String alignedApk = apkZipalign(v1Apk);
-
-        // Step 3: V2 + V3 signing
-        try {
-
-            KeyStore ks = KeyStore.getInstance("JKS");
-            try (FileInputStream fis = new FileInputStream(keyStore)) {
-                ks.load(fis, keyPwd.toCharArray());
-            }
-
-            PrivateKey key = (PrivateKey) ks.getKey(alias, alisaPwd.toCharArray());
-            Certificate[] certs = ks.getCertificateChain(alias);
-
-            ApkSigner.SignerConfig signerConfig =
-                    new ApkSigner.SignerConfig.Builder(alias, key, Collections.singletonList(certs[0]))
-                            .build();
-
-            ApkSigner signer = new ApkSigner.Builder(Collections.singletonList(signerConfig))
-                    .setInputApk(new File(alignedApk))
-                    .setOutputApk(signedApk)
-                    .setV1SigningEnabled(true)
-                    .setV2SigningEnabled(true)
-                    .setV3SigningEnabled(true)
-                    .build();
-
-            signer.sign();
-
-        } catch (Exception e) {
-            throw new IOException("V2+V3 signing failed", e);
-        }
-
-        // Cleanup
-        new File(alignedApk).delete();
-        new File(v1Apk).delete();
-
-        // Remove .idsig (v4 signature)
-        File idSig = new File(signedApk.getAbsolutePath() + ".idsig");
-        if (idSig.exists()) idSig.delete();
+        FileUtils.delete(zipalignName);
+        FileUtils.delete(signedApk.getAbsolutePath() + ".idsig");
     }
 }
